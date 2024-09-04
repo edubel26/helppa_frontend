54e8b538be834b7713671773787f93e90ba6122c
edubel26 <beltran502@hotmail.com>
2024-09-04 11:35:44 -0500

    cambios al apartado de creación de cuenta y login

--- js/form.js
+++ js/form.js
@@ -1,50 +1,63 @@
- // Ejecuta una sesión cuando carga la pagina
+ // Ejecuta una acción cuando carga la pagina
  document.addEventListener('DOMContentLoaded', function() {
 
-    //Obtenemos el id de la url
     const searchParams = new URLSearchParams(window.location.search)
+    const id = searchParams.get("id") // Asignamos el id a una variable
 
     // Acceder a la información de la etiqueta form
     const form = document.getElementById('login-form')
-    //Ejecuto una acción cuando el formulario se envía
+    // Ejecuto una accion cuando el formulario se envia
     form.addEventListener('submit', function(event){
-        event.preventDefault(); // Evita que el formulario recarga la pagina al enviar
-        //creo el objeto que voy a almacenar
+        event.preventDefault(); // Evita que el formulario recargue la pagina al enviar
+        // Creo el objeto que voy a almacenar
         const data = {
-
-            nombres: document.getElementById('name').value,
-            apellidos: document.getElementById('lastname').value,
+            nombres: document.getElementById('nombres').value,
+            apellidos: document.getElementById('apellidos').value,
             email: document.getElementById('email').value,
             password: document.getElementById('password').value
-     
         }
 
-        submitForm(data, "http://localhost:3000/usuarios/", "POST")
-
+        if(searchParams.has('id')){ // Si llega un id en la URL -> Actualizamos
+            submitForm(data, "http://localhost:3000/usuario"+id, "PUT")
+        }else{
+            submitForm(data, "http://localhost:3000/usuario", "POST")
+        }
     })
 
-
+    // Obtenemos el id de la url
+    if(searchParams.has('id')){ // verificamos si el parámetro id viene en la url
+        // Consulta la información del usuario que tiene ese id
+        fetch('http://localhost:3000/usuario'+id)
+        .then(response => response.json()) // Lo convierte a formato json
+        .then(data => {
+            // Asignamos la información al formulario
+            document.getElementById('nombres').value = data.nombres
+            document.getElementById('apellidos').value = data.apellidos
+            document.getElementById('email').value = data.email
+        })
+    }
 })
 
 function submitForm(data, url, method){
-    fetch(url, {
-            method: method, 
+
+    fetch(url, { // localhost:3000/usuario
+            method: method, // POST
             headers: {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify(data)
         })
         .then(response => {
-            return response.json()
+            return response.json()                
         })
         .then(data => {
-            if(data.status == "success"){
+            if(data.status == "success"){ // Validamos la respuesta del servidor
                 alert('Usuario registrado correctamente')
-                window.location.href = "inicioDeSesion.html";
+                window.location.href = "inicioDeSesion.html"; // Re diseccionamos a la lista
             }else{
-                alert('El usuario ya esta registrado');
+                alert('Error, el usuario ya esta registrado');
                 window.location.href = "inicioDeSesion.html";
             }
         })
-        .catch(error => console.log("Error: ", error))
+        .catch((error) => console.log("Error: ", error))
 }
